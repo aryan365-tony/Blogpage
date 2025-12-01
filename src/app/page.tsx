98ff9fa1666fd8bd8e1d94702c06a18a364eaad0
FACTOLOGY <aabc83754@gmail.com>
2025-12-01 15:09:44 +0000

    Same for blog posts images as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <main className="flex-1">
         <section className="relative w-full py-12 md:py-24 lg:py-32 bg-card border-b">
           <Image
-            src="/images/hero-background.jpg"
+            src="/hero-background.jpg"
             alt="Kakehashi Hub background"
             fill
             className="object-cover"
